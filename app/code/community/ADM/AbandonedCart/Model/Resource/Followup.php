4674ac1f764640a58387d45262032a64fa440706
Andrea Zambon <azambon@webgriffe.com>
2020-06-10 10:52:31 +0200

    Fixes abandoned cart enable/disable by store

--- app/code/community/ADM/AbandonedCart/Model/Resource/Followup.php
+++ app/code/community/ADM/AbandonedCart/Model/Resource/Followup.php
@@ -14,10 +14,10 @@ class ADM_AbandonedCart_Model_Resource_Followup extends Mage_Core_Model_Resource
 
         $quotes = Mage::getModel('sales/quote')->getCollection()
             ->addFieldToFilter('main_table.is_active', 1)
-            ->addFieldToFilter('main_table.items_count', array('gt'=>0))
+            ->addFieldToFilter('main_table.items_count', array('gt' => 0))
             ->addFieldToFilter('main_table.customer_email', array('notnull' => true))
-            ->addFieldToFilter('main_table.updated_at', array('lt'=>$this->_getDateSubTime($minDelay)))
-            ->addFieldToFilter('main_table.updated_at', array('gt'=>$this->_getDateSubTime($maxDelay)));
+            ->addFieldToFilter('main_table.updated_at', array('lt' => $this->_getDateSubTime($minDelay)))
+            ->addFieldToFilter('main_table.updated_at', array('gt' => $this->_getDateSubTime($maxDelay)));
 
         if(!$this->_getHelper()->followVirtual()) {
             $quotes->addFieldToFilter('main_table.is_virtual', 0);
@@ -26,32 +26,32 @@ class ADM_AbandonedCart_Model_Resource_Followup extends Mage_Core_Model_Resource
         $quotes->getSelect()->joinLeft(array('followup'=>$this->getMainTable()) , 'followup.quote_id=main_table.entity_id', array())
             ->where('followup.followup_id is null');
 
-
         Mage::dispatchEvent('adm_abandonedcart_quote_collection_load_before', array('collection' => $quotes));
 
+        $affectedRows = 0;
         if($quotes->getSize()>0) {
             $folloupRows = array();
             foreach($quotes as $quote) {
-                $folloupRows[] = array('quote_id'=>$quote->getId(),
-                        'store_id'=>$quote->getStoreId(),
-                        'abandoned_at'=>$quote->getUpdatedAt(),
-                        'customer_id'=>$quote->getCustomerId(),
-                        'customer_email'=>$quote->getCustomerEmail(),
-                        'secret_code'=>md5(uniqid()),
-                        'currency' => $quote->getQuoteCurrencyCode(),
-                        'base_grand_total'=>$quote->getBaseGrandTotal(),
-                        'coupon_code'=>$quote->getCouponCode(),
-                        'mail_scheduled_at'=>Mage::getModel('core/date')->gmtDate()
-                        );
+                if (Mage::helper('adm_abandonedcart')->isEnabled($quote->getStoreId())) {
+                    $folloupRows[] = array(
+                        'quote_id'          => $quote->getId(),
+                        'store_id'          => $quote->getStoreId(),
+                        'abandoned_at'      => $quote->getUpdatedAt(),
+                        'customer_id'       => $quote->getCustomerId(),
+                        'customer_email'    => $quote->getCustomerEmail(),
+                        'secret_code'       => md5(uniqid()),
+                        'currency'          => $quote->getQuoteCurrencyCode(),
+                        'base_grand_total'  => $quote->getBaseGrandTotal(),
+                        'coupon_code'       => $quote->getCouponCode(),
+                        'mail_scheduled_at' => Mage::getModel('core/date')->gmtDate()
+                    );
+                }
             }
-
-            $affectedRows = $this->_getWriteAdapter()->insertMultiple($this->getMainTable(),$folloupRows);
-        } else {
-            $affectedRows = 0;
+            if (count($folloupRows)) {
+                $affectedRows = $this->_getWriteAdapter()->insertMultiple($this->getMainTable(), $folloupRows);
+            }
         }
-
         return $affectedRows;
-
     }
 
     protected function _getDateSubTime($nbr, $type = Zend_Date::HOUR)
